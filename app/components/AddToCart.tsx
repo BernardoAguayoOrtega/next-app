290204b18bc14f846928be57987251c51867574c
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-02-28 19:18:37 -0600

    Add AddToCart component and import ProductCard in page.tsx

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+
+function AddToCart() {
+  return (
+    <>
+      <div>AddToCart</div>
+      <button onClick={() => {}}>Add to Cart</button>
+    </>
+  );
+}
+
+export default AddToCart;
