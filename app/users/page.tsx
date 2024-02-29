ca0a031cc31220c2e7e9c79f3c24d76cc402e32c
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-02-28 19:23:19 -0600

    Refactor UsersPage component to fetch and display user data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  const res = await fetch("http://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      {users.map((user: User) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default UsersPage;
