cf84e32813a185e799b519eb859e97c3f94ee06e
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-02-29 20:59:08 -0600

    Refactor ProductCard component styling

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import AddToCart from "./AddToCart";
-import styles from "./ProductCard.module.css";
 
 function ProductCard() {
   return (
-    <>
-      <div className={styles.card}>ProductCard</div>
+    <div className="p-5 my-5 bg-sky-300 text-white text-xl hover:bg-slate-300">
+      <h3>ProductCard</h3>
       <AddToCart />
-    </>
+    </div>
   );
 }
 
