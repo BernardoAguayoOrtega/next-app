ef2b2307405f06d2d3abad00fec1efe7c290b3f3
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 20:00:48 -0600

    Refactor layout components

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -6,11 +6,8 @@ export default function GeneralLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Navbar />
-      <div className='flex flex-col items-center p-24  bg-gradient-to-r from-indigo-100'>
-        {children}
-      </div>
-    </>
+    <div className='flex flex-col items-center p-24  bg-gradient-to-r from-indigo-100'>
+      {children}
+    </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Navbar } from "./components";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Bernardo Aguayo',
-  description: 'personal project to learn Next.js and TypeScript',
-  robots: 'follow, index',
-}
+  title: "Bernardo Aguayo",
+  description: "personal project to learn Next.js and TypeScript",
+  robots: "follow, index",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="winter">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' data-theme='winter'>
+      <body className={inter.className}>
+        <>
+          <Navbar />
+          {children}
+        </>
+      </body>
     </html>
-  )
+  );
 }
