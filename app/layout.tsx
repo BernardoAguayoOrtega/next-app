391c9f1ec1eb040df54fcbdfb2281998e0169903
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 18:46:38 -0600

    Add robots meta tag to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Bernardo Aguayo',
   description: 'personal project to learn Next.js and TypeScript',
+  robots: 'follow, index',
 }
 
 export default function RootLayout({
