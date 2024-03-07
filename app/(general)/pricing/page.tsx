d834640c6561d217e21bb7b3fd0732bd3542cea9
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 19:53:07 -0600

    Add pricing page and update navbar links

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+import React from 'react'
+
+
+export const metadata: Metadata = {
+ title: 'Pricing page',
+  description: 'Pricing page',
+};
+
+function Pricing() {
+  return (
+    <div>Pricing</div>
+  )
+}
+
+export default Pricing
--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,21 +1,15 @@
+import Link from "next/link";
+
 type Props = {};
 
 export function Navbar({}: Props) {
   return (
     <nav className='flex bg-blue-100 bg-opacity-30 p-2 m-2 rounded'>
-      <ul className='flex space-x-4'>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/about'>About</a>
-        </li>
-        <li>
-          <a href='/contact'>Contact</a>
-        </li>
-        <li>
-          <a href='/pricing'>Pricing</a>
-        </li>
+      <ul className='flex space-x-6'>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/contact'>Contact</Link>
+        <Link href='/pricing'>Pricing</Link>
       </ul>
     </nav>
   );
