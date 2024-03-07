4d8e305f455c02173faf70492f799f7c926c53a8
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 19:40:51 -0600

    Add Navbar component to GeneralLayout

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,11 +1,16 @@
+import { Navbar } from "@/app/components";
+
 export default function GeneralLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className='flex flex-col items-center p-24  bg-gradient-to-r from-indigo-100'>
-      {children}
-    </div>
+    <>
+      <Navbar />
+      <div className='flex flex-col items-center p-24  bg-gradient-to-r from-indigo-100'>
+        {children}
+      </div>
+    </>
   );
 }
--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 type Props = {};
 
-export default function Navbar({}: Props) {
+export function Navbar({}: Props) {
   return (
     <nav className='flex bg-blue-100 bg-opacity-30 p-2 m-2 rounded'>Navbar</nav>
   );
