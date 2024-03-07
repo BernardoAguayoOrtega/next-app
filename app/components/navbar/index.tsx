34a309744eea98695c660202204e0ccf1d542892
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 20:03:55 -0600

    Add HomeIcon to Navbar component and update dependencies

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
 
 type Props = {};
@@ -6,7 +7,10 @@ export function Navbar({}: Props) {
   return (
     <nav className='flex bg-blue-100 bg-opacity-30 p-2 m-2 rounded'>
       <ul className='flex space-x-6'>
-        <Link href='/'>Home</Link>
+        <Link href='/'>
+          <HomeIcon size={24} />
+          <span className='sr-only'>Home</span>
+        </Link>
         <Link href='/about'>About</Link>
         <Link href='/contact'>Contact</Link>
         <Link href='/pricing'>Pricing</Link>
