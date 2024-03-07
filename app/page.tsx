bd6e6f57657a2098e5b0c8e1cd5b2b613fd6580d
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-06 19:21:44 -0600

    Remove unused components and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link'
-import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>Next.js + TypeScript</h1>
       <Link href="/users">Users</Link>
-      <ProductCard />
     </main>
   )
 }
