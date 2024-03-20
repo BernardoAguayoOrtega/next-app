f639e4827f8e063d4e3d892fe0ddef7918b7dc03
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-03-19 18:59:00 -0600

    Add metadata for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Main page",
+  description: "This is the main page of the Next.js + TypeScript app",
+};
+
 export default function Home() {
   return (
     <main>
